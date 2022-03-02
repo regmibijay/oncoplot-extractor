faa622a37f7d562ed46200bb12062c67be323638
Bijay Regmi <bijayregmi5@gmail.com>
2022-03-02 13:59:35 +0100

    improved documentation

--- oncoplot_extractor/main.py
+++ oncoplot_extractor/main.py
@@ -1,3 +1,5 @@
+"""Contains OncoPlotExtractor class."""
+
 import json
 import os
 from typing import DefaultDict
@@ -5,12 +7,25 @@ from typing import DefaultDict
 import pandas as pd
 from PIL import Image
 
-
 from .errors import OncoPlotExtractorError
 
 
 class OncoplotExtractor:
-    """Extracts oncoplot data from oncoplot image and exports to excel."""
+    """Extracts oncoplot data from oncoplot image and exports to excel.
+
+    :param path: path to oncoplot image [str] : required
+    :param corners: (x1, y1, x2, y2) top left and bottom right corner of oncoplot [tuple] : optional
+    :param background_color: background color of oncoplot [list] : optional
+    :param gene_list: list of genes to extract [list] : optional
+
+    :Example:
+    .. code-block:: python
+
+        oce = OncoplotExtractor(path="path/to/oncoplot.png", corners=(0,0,100,100), background_color=["#ffffff"])
+        oce.extract()
+        oce.export_to_excel("path/to/oncoplot.xlsx")
+
+    """
 
     FILEPATH: str
     CORNERS: tuple
@@ -38,23 +53,30 @@ class OncoplotExtractor:
         self.GENE_LIST = gene_list
 
     def _check_if_file_exists(self, path):
-        "Checks if file exists at `path`."
+        """Checks if file exists at `path`."""
         return os.path.isfile(path)
 
     def _get_rgb_image_data(self) -> Image.Image:
-        "Returns image data from `self.FILEPATH`."
+        """Returns image data from `self.FILEPATH`."""
         return Image.open(self.FILEPATH).convert("RGB")
 
     def _crop_image(self, img: Image.Image, corners: tuple) -> Image.Image:
-        "Crops image to oncoplot."
+        """Crops image to oncoplot.
+        :param img: image to crop
+        :param corners: (x1, y1, x2, y2) top left and bottom right corner of oncoplot
+        """
         return img.crop(corners)
 
     def _get_color_name_from_rgb(self, rgb: tuple) -> str:
-        "Returns color name from rgb tuple format (r,g,b)."
+        """Returns color hex code from
+        :param name:rgb tuple format (r,g,b).
+        """
         return "#{:02x}{:02x}{:02x}".format(*rgb)
 
     def _extract_row_poi(self, padding_left: int = 0) -> list:
-        "Extracts row poi from oncoplot image."
+        """Extracts row poi from oncoplot image.
+        :param name:padding_left number of pixels to ignore left of image.
+        :returns: list of row poi"""
         row_poi = []
         last_color = None
         for h in range(0, self.IMG.height):
@@ -67,7 +89,10 @@ class OncoplotExtractor:
         return row_poi
 
     def _extract_col_poi(self, row_pos: int) -> list:
-        "Extracts column poi from oncoplot image."
+        """
+        Extracts column poi from oncoplot image.
+        :param row_pos: row position of row poi
+        :returns: list of column poi"""
         col_poi = []
         last_color = None
         for w in range(0, self.IMG.width):
@@ -81,7 +106,9 @@ class OncoplotExtractor:
         return col_poi
 
     def extract(self) -> None:
-        "Extracts oncoplot data from image."
+        """Extracts oncoplot data from image.
+        expects class to have been initialized with a valid image file
+        """
         if self.CORNERS is not None:
             self.IMG = self._crop_image(self.IMG, self.CORNERS)
         rows = self._extract_row_poi()
@@ -92,7 +119,7 @@ class OncoplotExtractor:
 
     @property
     def as_dict(self) -> DefaultDict:
-        "Returns oncoplot data as dictionary."
+        """Returns oncoplot data as dictionary."""
         if len(self.DATA_AS_DICT) == 0:
             raise OncoPlotExtractorError(
                 """
@@ -104,29 +131,34 @@ class OncoplotExtractor:
 
     @property
     def as_dataframe(self) -> pd.DataFrame:
-        "Returns oncoplot data as pandas dataframe."
+        """Returns oncoplot data as pandas dataframe."""
         if self.GENE_LIST is not None:
             index = pd.Index(self.GENE_LIST)
             return pd.DataFrame(self.as_dict, index=index)
         return pd.DataFrame(self.as_dict)
 
     def export_to_excel(self, filepath: str) -> None:
-        "Exports oncoplot data to excel."
+        """Exports oncoplot data to excel.
+        :param filepath: path to excel file to export to.
+        """
         self.as_dataframe.to_excel(filepath)
 
     def export_to_json(self, filepath: str) -> None:
-        "Exports oncoplot data to json."
+        """Exports oncoplot data to json.
+        :param filepath: path to json file to export to."""
         with open(filepath, "w") as f:
             json.dump(self.as_dict, f)
 
-    def pixel_to_hex(self, coord):
-        "Returns hex value of pixel at `coord`."
+    def pixel_to_hex(self, coord) -> str:
+        """Returns hex value of pixel at `coord`."""
         return self._get_color_name_from_rgb(self.IMG.getpixel(coord))
 
     def background_color(self, coord: tuple) -> list:
-        "Returns background color of pixel at `coord`."
+        """Returns background color of pixel at `coord`.
+        :param coord: (x,y) coordinate of pixel"""
         return [self.pixel_to_hex(coord)]
 
     def get_background_from_pixel(self, coord: tuple) -> None:
-        "sets background color of image at `coord` so this pixel is filtered."
+        """sets background color of image at `coord` so this pixel is filtered.
+        :param coord: (x,y) coordinate of pixel"""
         self.BG_COLOR = self.background_color(coord)
--- oncoplot_extractor/plotter.py
+++ oncoplot_extractor/plotter.py
@@ -1,13 +1,27 @@
 "Base file containing OncoPlotCreator"
 
-import pandas as pd
 import openpyxl
+import pandas as pd
 from openpyxl.utils.dataframe import dataframe_to_rows
 
 
 class OncoPlotCreator:
-    """Tries to create an oncoplot from a dataframe
-    containing respective rows and columns with hexcolor data"""
+    """Creates an oncoplot from a dataframe
+    containing respective rows and columns with hexcolor data
+
+    :param df: pandas dataframe containing hexcolor data [pandas.DataFrame]
+    :param cell_size: size of each cell in pixels [int]
+    :param workbook: openpyxl workbook object [openpyxl.Workbook]
+    :param offset: number of rows to offset inserted data [int]
+
+    :Example:
+    .. code-block:: python
+
+        opc = OncoPlotCreator(df=my_df, cell_size=60, workbook=wb, offset=10)
+        opc.gen_base_oncoplot()
+        opc.save(filename="my_oncoplot.xlsx")
+
+    """
 
     CELL_SIZE: int
     OUT_PATH: str = None
@@ -33,7 +47,7 @@ class OncoPlotCreator:
         self._load_df_to_ws()
 
     def _load_df_to_ws(self):
-        "loads self.DF to self.WS"
+        """loads self.DF to self.WS"""
         for row in dataframe_to_rows(self.DF, index=True):
             self.WORKSHEET.append(row)
 
@@ -42,6 +56,9 @@ class OncoPlotCreator:
             self.WORKSHEET.insert_rows(0)
 
     def gen_base_oncoplot(self):
+        """Generates base oncoplot from self.DF
+        expects self.DF to be loaded to self.WS
+        """
         thin = openpyxl.styles.Side(border_style="thin", color="ffffff")
         self._insert_offset()
         _c = 1
@@ -59,5 +76,7 @@ class OncoPlotCreator:
             _c += 1
 
     def save(self, filename: str) -> None:
-        "saves generated workbook to file"
+        """saves generated workbook to file
+        :param filename: filename to save workbook to [str]
+        """
         self.WORKBOOK.save(filename=filename)
