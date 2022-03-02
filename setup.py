ab68fda1e07cc6f348af6a8c5361e67346fd561e
regmibijay <bijayregmi5@gmail.com>
2022-03-02 23:48:06 +0100

    adds pip package builder

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+setuptools.setup(
+    name="oncoplot_extractor",
+    version="1.0.1",
+    packages=setuptools.find_packages(),
+    url="https://github.com/regmibijay/oncoplot-extractor",
+    license="GNU (GPLv3)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Bijay Regmi",
+    author_email="oncoplot-extractor@regdelivery.de",
+    description="General Toolkit for OncoPlot written in Python",
+    install_requires=requirements,
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
