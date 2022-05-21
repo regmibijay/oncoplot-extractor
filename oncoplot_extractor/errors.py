a1326681c1ac49717ab63e0fafe23114c3417d08
regmibijay <bijayregmi5@gmail.com>
2022-05-21 23:57:43 +0200

    adds error class for plotter

--- oncoplot_extractor/errors.py
+++ oncoplot_extractor/errors.py
@@ -6,3 +6,10 @@ class OncoPlotExtractorError(Exception):
 
     def __init__(self, msg) -> None:
         super().__init__(msg)
+
+
+class OncoPlotPlotterError(Exception):
+    """Base class for all OncoPlot Plotter errors."""
+
+    def __init__(self, msg) -> None:
+        super().__init__(msg)
